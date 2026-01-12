4de5813d613fca35de753ed156dcd4ad8e68442f
NABAJYOTI <nabajyotinag609@gmail.com>
2026-01-12 15:07:17 +0530

    Initial commit

---
+++ EVEN_OR_ODD.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a Integer:"))
+
+if num % 2 == 0:
+    print(f"{num} is an EVEN Number.")
+else:
+    print(f"{num} is an ODD Number.")
---
+++ sum_of_1 to 50.py
@@ -0,0 +1,4 @@
+s = 0
+for num in range(1,51):
+    s = s + num
+print("The sum of numbers from 1 to 50 is:",s)
