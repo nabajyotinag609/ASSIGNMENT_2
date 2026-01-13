5c97ad00c7a655a93c7c4e3e46e874d0e2315c7e
nabajyotinag609 <nabajyotinag609@gmail.com>
2026-01-13 21:22:22 +0530

    Delete A Personalized Greeting.py

--- A Personalized Greeting.py
+++
@@ -1,4 +0,0 @@
-first_name = input("Enter your First name: ")
-last_name = input("Enter your Last name: ")
-full_name = first_name + " " + last_name
-print("Hello, ",full_name,"! Welcome to the my project.",sep="")
